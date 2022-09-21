583918954de264d48954ab4ee18de02449523859
EnriqueTerrones <enrique.terrones.nava@gmail.com>
2022-09-21 18:40:55 -0500

    team2 activity

--- src/js/product.js
+++ src/js/product.js
@@ -1,35 +1,48 @@
-let products = [];
-function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
-    throw new Error("Bad Response");
-  }
-}
+import ProductData from './productData.js';
+import ProductDetails from './productDetails.js';
+import { getParam } from './utils.js';
 
-function setLocalStorage(key, data) {
-  localStorage.setItem(key, JSON.stringify(data));
-}
+const productId = getParam('product');
+const dataSource = new ProductData('tents');
 
-// get tents data
-function getProductsData() {
-  fetch("../json/tents.json")
-    .then(convertToJson)
-    .then((data) => {
-      products = data;
-    });
-}
-// or should we do it this way?
-// async function getProductsDataAwait() {
-//   products = await fetch("../json/tents.json").then(convertToJson);
+const product = new ProductDetails(productId, dataSource);
+product.init();
+
+
+// let products = [];
+
+// // //function convertToJson(res) {
+// // //  if (res.ok) {
+// //   //  return res.json();
+// // //  } else {
+// //   //  throw new Error("Bad Response");
+// //   }
+// // }
+// //
+// function setLocalStorage(key, data) {
+//   localStorage.setItem(key, JSON.stringify(data));
 // }
 
-// add to cart button event handler
-function addToCart(e) {
-  const product = products.find((item) => item.Id === e.target.dataset.id);
-  setLocalStorage("so-cart", product);
-}
+// // get tents data
+// //function getProductsData() {
+// //   fetch("../json/tents.json")
+// //     .then(convertToJson)
+// //     .then((data) => {
+// //       products = data;
+// //     });
+// // }
+// // or should we do it this way?
+// // async function getProductsDataAwait() {
+// //   products = await fetch("../json/tents.json").then(convertToJson);
+// // }
 
-getProductsData();
-// add listener to Add to Cart button
-document.getElementById("addToCart").addEventListener("click", addToCart);
+// // add to cart button event handler
+// function addToCart(e) {
+//   // const product = products.find((item) => item.Id === e.target.dataset.id);
+//   setLocalStorage("so-cart", product);
+// }
+
+// getProductsData();
+// // add listener to Add to Cart button
+// document.getElementById("addToCart").addEventListener("click", addToCart);
+
