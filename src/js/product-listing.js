ef5d5fdce36018a71bf0dc9ebe12c82877a242ff
EnriqueTerrones <enrique.terrones.nava@gmail.com>
2022-09-29 21:45:14 -0500

    product error

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -1,5 +1,21 @@
+// import ProductData from "./productData.js";
+// import ProductList from "./productList.js";
+
+// const dataSourceProd = new ProductData("tents");
+// const dataSourcePList = new ProductList("tents", "", "");
+
 import ProductData from "./productData.js";
 import ProductList from "./productList.js";
+import { loadHeaderFooter, getParam } from "./utils.js";
 
-const dataSourceProd = new ProductData("tents");
-const dataSourcePList = new ProductList("tents", "", "");
+loadHeaderFooter();
+
+const category = getParam("category");
+// first create an instance of our ProductData class.
+const dataSource = new ProductData();
+// then get the element we want the product list to render in
+const listElement = document.querySelector(".product-list");
+// then create an instance of our ProductList class and send it the correct information.
+const myList = new ProductList(category, dataSource, listElement);
+// finally call the init method to show our products
+myList.init();
