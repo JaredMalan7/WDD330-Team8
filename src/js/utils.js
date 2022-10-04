0110e39f517986153666903d934722106de74d4e
thecef02 <83976385+thecef02@users.noreply.github.com>
2022-10-04 15:52:02 -0600

    stage all

--- src/js/utils.js
+++ src/js/utils.js
@@ -59,8 +59,8 @@ export async function loadTemplate(path) {
 
 // load the header and footer
 export async function loadHeaderFooter() {
-  const header = await loadTemplate("../partials/header.html");
-  const footer = await loadTemplate("../partials/footer.html");
+  const header = await loadTemplate("../src/partials/header.html");
+  const footer = await loadTemplate("../src/partials/footer.html");
   const headerElement = document.getElementById("main-header");
   const footerElement = document.getElementById("main-footer");
   renderWithTemplate(header, headerElement);
